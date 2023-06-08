043c6c8fe5fccb4613aa6881c384798ad8f85168
Sofiguzmanc <sofi.guzmanc@gmail.com>
2023-06-08 12:29:18 -0300

    Uniendo app.js con home.html

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
 const express = require ("express");
 
 const app = express ();
-const port = 3003;
+const port = 3002;
 
 const path =  require("path");
 
+console.log(path.join(__dirname, "views", "index.html"));
+
 app.get ("/",(req,res) => {
-    res.sendFile(__dirname, "views", "index.html")
+    res.sendFile(__dirname, "views", "home.html")
 })
-
-app.get ("/register",(req,res) => {
-    res.send("Crea tu cuenta")
-})
-
-app.get ("/login",(req,res) => {
-    res.send("Ingresa a tu cuenta")
-})
-
 
 app.listen (port, () => {
     console.log("Servidor corriendo en el puerto" + port);
